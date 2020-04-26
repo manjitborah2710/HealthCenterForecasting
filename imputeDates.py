981e5be6248a5eb6a07439f4be2b696c9bf7fe15
ManjiT <manjitborah2710.com>
2020-04-26 18:38:30 +0530

    added code for provideing weekly and monthly details

--- imputeDates.py
+++ imputeDates.py
@@ -2,18 +2,24 @@ import sys
 import pandas as pd
 from pandas import datetime
 def parser(x):
-    return datetime.strptime(x,'%d-%m-%Y')
+    global parse_format
+    return datetime.strptime(x,parse_format)
 
 args=sys.argv
-if not (len(args)==4):
-    print("No input provided..specify file_name, start date and end date separated by spaces")
+if not (len(args)>=4):
+    print("No input provided..specify file_name, start date, end date, parse_format(optional, default='%d-%m-%Y') separated by spaces")
     sys.exit()
 
 print(args)
+parse_format='%d-%m-%Y'
 
 file=args[1]
 start_date=args[2]
 end_date=args[3]
+
+if len(args)==5:
+    parse_format=args[4]
+
 df=pd.read_csv(file,index_col=0,date_parser=parser,parse_dates=[0])
 df.index=pd.DatetimeIndex(df.index)
 idx=pd.date_range(start_date,end_date,name="Date")
@@ -21,3 +27,4 @@ df=df.reindex(idx,fill_value=0)
 name="DATES_FILLED_"+file
 df.to_csv(name)
 print("File generated : ",name)
+
